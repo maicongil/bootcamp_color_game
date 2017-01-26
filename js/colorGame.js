96765461d9040e7ab8380d59a3a62f0f5943786c
maicon <gugamaicon@gmail.com>
2017-01-25 22:02:31 -0200

    Pt4 - Implemented random colors creation

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,16 +1,10 @@
-var colors =[
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+var colors = generateColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,6 +15,7 @@ for(var i=0; i<squares.length;i++){
         var clickedColor = this.style.background;
         if(clickedColor === pickedColor){
             message.textContent = "Correct!";
+            h1.style.background = clickedColor;
             changeColors(clickedColor);
         }else{
             this.style.background = "#232323";
@@ -38,4 +33,19 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateColors(quantity){
+    var generatedColors =[];
+    for(var i = 0; i< quantity; i++){
+        generatedColors.push(generateColor());
+    }
+    return generatedColors;
+}
+
+function generateColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb("+r+", "+g+", "+b+")";
 }
